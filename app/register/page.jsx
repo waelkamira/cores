f223ef2583bdffcf2cfc05f509fd02f4f8522a97
waelkamira <ramondm4@gmail.com>
2024-03-15 18:05:38 +0300

    first commit

---
+++ app/register/page.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+export default function RegisterPage() {
+  return <div></div>;
+}
